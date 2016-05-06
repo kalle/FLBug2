347ea4e1a1973312068e45f4e6483f2ecd6a3d8c
Kalle Svensson <kalle@boldjump.com>
2016-05-06 11:36:54 +0200

    Initial commit

---
+++ FLbug2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FLbug2
+//
+//  Created by kalle on 2016-05-06.
+//  Copyright © 2016 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
